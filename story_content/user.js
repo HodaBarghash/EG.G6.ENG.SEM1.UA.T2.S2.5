9be635b5b3a7472a563b9f498029a0fa6afb5a02
HodaBarghash <121548764+HodaBarghash@users.noreply.github.com>
2023-10-05 19:20:17 +0300

    Initial commit

---
+++ story_content/user.js
@@ -0,0 +1,27 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6YgB56xmHVY":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  const url="https://script.google.com/macros/s/AKfycbz5TzbdgUekQ8_0J8V74HpRevX4Ly_mVGqZe54ICfe5FXvXMyWv09smGrDIWnO_9pda/exec"
+
+var player=GetPlayer()
+
+fetch(url,{
+	method:"POST",
+	mode: 'no-cors',
+	cache:'no-cache',
+	headers: {'Content-Type': 'application/json'},
+	redirect:'follow',
+	body: JSON.stringify({v14q1attempts:player.GetVar("v14q1attempts"),v14q1answeredcorrect:player.GetVar("v14q1answeredcorrect"),v14q2attempts:player.GetVar("v14q2attempts"),v14q2answeredcorrect:player.GetVar("v14q2answeredcorrect"),v14q3attempts:player.GetVar("v14q3attempts"),v14q3answeredcorrect:player.GetVar("v14q3answeredcorrect")})
+	}
+	)
+}
+
